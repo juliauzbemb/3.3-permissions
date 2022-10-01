d5a94af8def0b737de105de54568d20fecb7561c
Yulia <juliauzbemb@mail.ru>
2022-10-01 22:20:50 +0300

    initial commit

---
+++ api_with_restrictions/advertisements/filters.py
@@ -0,0 +1,16 @@
+from django_filters import rest_framework as filters
+
+from .models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+
+    # TODO: задайте требуемые фильтры
+    created_at = filters.DateFromToRangeFilter()
+    updated_at = filters.DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['status', 'creator', 'created_at']
+
